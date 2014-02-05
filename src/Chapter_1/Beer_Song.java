687eb7a10ccfdceb6efea1f950f9fa321facda98
F-l-a-m <derlowe18@gmail.com>
2014-02-05 12:36:05 +0200

    Beer Song, Chapter_1_p_20

---
+++ src/Chapter_1/Beer_Song.java
@@ -0,0 +1,29 @@
+package Chapter_1;
+
+public class Beer_Song {
+
+	public static void main(String[] args) {
+		int beerNum = 5;
+		String word = "bottles";
+		while (beerNum > 0) {
+			if (beerNum == 1) {
+				word = "bottle"; // singular, as in ONE bottle.
+			}
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around.");
+			beerNum = beerNum - 1;
+			if (beerNum > 0) {
+				if (beerNum == 1) {
+					word = "bottle"; // singular, as in ONE bottle.
+				}
+				System.out.println(beerNum + " " + word + " of beer on the wall");
+				System.out.println(" ");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+				System.out.println(" ");
+			} // end else
+		} // end while loop
+	}
+}
